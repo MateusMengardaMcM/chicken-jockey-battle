3cd7351dc350cb08d5ed84f3fb931c043f0d6a59
Mateus Mengarda <mateus.byjus@gmail.com>
2026-03-23 18:52:18 -0300

    vou subir pro github para a prof checar o código

---
+++ script.js
@@ -0,0 +1,59 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 500,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {y:1},
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+const game = new Phaser.Game(config);
+
+let player;
+let enemy;
+let cursors;
+
+function preload() {
+    this.load.image('player','player.png');
+    this.load.image('enemy','enemy.png');
+    this.load.image('bg','background.png');
+}
+
+function create() {
+    this.add.image(400,300,"bg").setDisplaySize(800,500);
+    this.ground = this.add.rectangle(400,500,800,40);
+    this.physics.add.existing(this.ground,true);
+    this.ground.setVisible(false);
+
+    player = this.physics.add.sprite(100,360,'player');
+    player.setScale(0.075);
+    this.physics.add.collider(this.player,this.ground);
+
+    enemy = this.physics.add.sprite(700,400,'enemy');
+    enemy.setScale(0.5);
+
+    cursors = this.input.keyboard.createCursorKeys();
+}
+
+function update() {
+    if (cursors.left.isDown) {
+        player.x -= 3;
+    } else if (cursors.right.isDown) {
+        player.x += 3;
+    }
+
+    if (cursors.up.isDown) {
+        player.y -= 3;
+    } else if (cursors.down.isDown) {
+        player.y += 3;
+    }
+}
